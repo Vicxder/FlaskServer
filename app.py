a4c947b4f1a695abd054ebf84ea1e9356e364aa0
Vicxder <Vicxder@gmail.com>
2025-01-16 20:36:41 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,21 @@
 from flask import Flask, request, jsonify
-from flask_cors import CORS  # Importar Flask-CORS
+from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)  # Habilitar CORS para todas las rutas
 
-@app.route('/control', methods=['POST'])
+@app.route("/control", methods=["POST", "OPTIONS"])
 def control():
-    data = request.json  # Recibir datos JSON del frontend
-    command = data.get('command')
-    if command:
+    if request.method == "POST":
+        data = request.json
+        command = data.get("command", "")
         print(f"Comando recibido: {command}")
-        return jsonify({"status": "success", "command": command}), 200
-    else:
-        return jsonify({"status": "error", "message": "Comando no proporcionado"}), 400
+        return jsonify({"message": "Comando recibido", "command": command})
+    return "OK", 200
 
-if __name__ == '__main__':
+@app.route("/")
+def home():
+    return "Servidor Flask Activo"
+
+if __name__ == "__main__":
     app.run(debug=True)
